f2266ddfc40b756caf86d7f2dd9d2b701f7c4dba
anthonybellon <anthonybellonparis@gmail.com>
2024-06-09 11:44:55 +0200

    feat: train model that went through all the stations plus deleted file no longer needed.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,188 +1,202 @@
+import os
 import pandas as pd
 import numpy as np
-import os
 import pickle
-from tqdm import tqdm
 import json
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_score, KFold
 from sklearn.preprocessing import StandardScaler
-from scipy.spatial import KDTree
-from collections import defaultdict
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from scipy.stats import randint
+from tqdm import tqdm
+from data_preprocessing import get_absolute_path
+from utils import calculate_nearby_station_status_adjustable
 
-def get_absolute_path(relative_path):
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), relative_path))
+def load_feature_names():
+    feature_name_path = get_absolute_path('../data/feature_names.json')
+    with open(feature_name_path, 'r') as file:
+        feature_names = json.load(file)
+    return feature_names
 
-def calculate_nearby_station_status_adjustable(data, station, initial_radius=500, max_radius=2000, increment=500):
-    station_data = data[data['stationcode'] == station]
-    if station_data.empty:
-        return None, station_data
+def check_for_large_values(data):
+    numeric_data = data.select_dtypes(include=[np.number])
+    large_values = (numeric_data.abs() > 1e10).any()
+    if large_values.any():
+        print("Large values found in columns during feature creation:", numeric_data.columns[large_values].tolist())
+    return large_values.any()
 
-    unique_coords = data[['lat', 'lon']].drop_duplicates().values
-    station_codes = data[['stationcode']].drop_duplicates().values.flatten()
-    kd_tree = KDTree(unique_coords)
+def create_features(data):
+    print ("Creating features...")
+    data = data.copy()
+    data['hour'] = data['date'].dt.hour
+    data['day_of_week'] = data['date'].dt.dayofweek
 
-    station_coords = station_data[['lat', 'lon']].iloc[0].values
-    target_station_code = station_data['stationcode'].iloc[0]
+    data = data.sort_values(by=['stationcode', 'date'])
+    data['lag_1_hour'] = data.groupby('stationcode')['numbikesavailable'].shift(1)
+    data['lag_1_day'] = data.groupby('stationcode')['numbikesavailable'].shift(24)
 
-    radius = initial_radius
-    while (radius <= max_radius):
-        indices = kd_tree.query_ball_point(station_coords, radius / 1000.0 / 111.32)
-        out_of_bounds_indices = [i for i in indices if i >= len(station_codes)]
-        
-        if out_of_bounds_indices:
-            print(f"Out of bounds indices for station {station}: {out_of_bounds_indices}")
-            print(f"Max valid index: {len(station_codes) - 1}")
-        
-        indices = [i for i in indices if i < len(station_codes)]
-        
-        print(f"Valid indices for station {station}: {indices}")
-        
-        nearby_indices = [i for i in indices if station_codes[i] != target_station_code]
+    data['rolling_mean_7_days'] = data.groupby('stationcode')['numbikesavailable'].transform(lambda x: x.rolling(window=7*24, min_periods=1).mean())
+    data['rolling_mean_30_days'] = data.groupby('stationcode')['numbikesavailable'].transform(lambda x: x.rolling(window=30*24, min_periods=1).mean())
 
-        nearby_stations = data.iloc[nearby_indices]
-        nearby_stations = nearby_stations[nearby_stations['is_installed'] != 'NON']
+    # Add small constants to avoid division by zero
+    data['normalized_bikes_available'] = data['numbikesavailable'] / (data['capacity'] + 1e-5)
+    data['normalized_docks_available'] = data['numdocksavailable'] / (data['capacity'] + 1e-5)
+    data['usage_ratio'] = data['numbikesavailable'] / (data['capacity'] + 1e-5)
+    data['capacity_hour_interaction'] = data['capacity'] * data['hour']
+    data['capacity_day_interaction'] = data['capacity'] * data['day_of_week']
 
-        if len(nearby_stations) >= 5:
-            return nearby_stations, station_data
+    data['avg_bikes_hour_day'] = data.groupby(['stationcode', 'hour', 'day_of_week'])['numbikesavailable'].transform('mean')
 
-        radius += increment
+    # Check for large values after feature creation
+    if check_for_large_values(data):
+        raise ValueError("Large values found in feature creation")
 
-    return nearby_stations, station_data
+    return data
 
-print("Loading current bike data...")
-current_data_path = get_absolute_path('../data/historical_data_cleaned/2021-04.json')
-with open(current_data_path, 'r') as f:
-    current_data = json.load(f)
+def train_model(data, stations, target='numbikesavailable'):
+    print("Training models...")
 
-current_bike_data = pd.DataFrame(current_data)
+    # Create necessary features
+    data = create_features(data)
+    
+    # Debug: Print columns to ensure features are created
+    print("Columns after feature creation:", data.columns)
 
-print("Preprocessing current bike data...")
-current_bike_data['date'] = pd.to_datetime(current_bike_data['duedate'])
+    # Normalize features
+    scaler = StandardScaler()
+    
+    # Load feature names from JSON
+    feature_names = load_feature_names()
+    base_features = feature_names[:12]
+    additional_features = feature_names[12:]
 
-if current_bike_data['date'].dt.tz is None:
-    current_bike_data['date'] = current_bike_data['date'].dt.tz_localize('UTC')
+    models = {}
+    param_dist = {
+        'n_estimators': randint(50, 200),
+        'max_depth': [10, 20, None],
+        'min_samples_split': randint(2, 10),
+        'min_samples_leaf': randint(1, 4)
+    }
 
-coords = pd.json_normalize(current_bike_data['coordonnees_geo'])
-current_bike_data['lat'] = coords['lat']
-current_bike_data['lon'] = coords['lon']
+    for station in tqdm(stations, desc="Training models", unit="station"):
+        nearby_stations, station_data = calculate_nearby_station_status_adjustable(data, station)
 
-current_bike_data.fillna(0, inplace=True)
-
-current_bike_data['hour'] = current_bike_data['date'].dt.hour
-current_bike_data['day_of_week'] = current_bike_data['date'].dt.dayofweek
-
-current_bike_data['hour_unscaled'] = current_bike_data['date'].dt.hour
-current_bike_data['day_of_week_unscaled'] = current_bike_data['date'].dt.dayofweek
-
-print("Adding lag features...")
-current_bike_data = current_bike_data.sort_values(by=['stationcode', 'date'])
-current_bike_data['lag_1_hour'] = current_bike_data.groupby('stationcode')['numbikesavailable'].shift(1)
-current_bike_data['lag_1_day'] = current_bike_data.groupby('stationcode')['numbikesavailable'].shift(24)
-
-print("Adding trend features...")
-current_bike_data['rolling_mean_7_days'] = current_bike_data.groupby('stationcode')['numbikesavailable'].transform(lambda x: x.rolling(window=7*24, min_periods=1).mean())
-current_bike_data['rolling_mean_30_days'] = current_bike_data.groupby('stationcode')['numbikesavailable'].transform(lambda x: x.rolling(window=30*24, min_periods=1).mean())
-
-current_bike_data['normalized_bikes_available'] = current_bike_data.apply(lambda row: row['numbikesavailable'] / row['capacity'] if row['capacity'] != 0 else 0, axis=1)
-current_bike_data['normalized_docks_available'] = current_bike_data.apply(lambda row: row['numdocksavailable'] / row['capacity'] if row['capacity'] != 0 else 0, axis=1)
-current_bike_data['usage_ratio'] = current_bike_data.apply(lambda row: row['numbikesavailable'] / (row['capacity'] + 1e-5), axis=1)
-current_bike_data['capacity_hour_interaction'] = current_bike_data['capacity'] * current_bike_data['hour']
-current_bike_data['capacity_day_interaction'] = current_bike_data['capacity'] * current_bike_data['day_of_week']
-
-print("Calculating nearby station status...")
-current_bike_data['nearby_stations_closed'] = 0
-current_bike_data['nearby_stations_full'] = 0
-current_bike_data['nearby_stations_empty'] = 0
-current_bike_data['likelihood_fill'] = 0.0
-current_bike_data['likelihood_empty'] = 0.0
-
-for station in tqdm(current_bike_data['stationcode'].unique(), desc="Calculating nearby station status"):
-    nearby_stations, station_data = calculate_nearby_station_status_adjustable(current_bike_data, station)
-    if nearby_stations is None:
-        continue
-    for index, row in station_data.iterrows():
-        date_filtered_nearby_stations = nearby_stations[nearby_stations['date'] == row['date']]
-        nearby_stations_closed = date_filtered_nearby_stations['is_installed'].apply(lambda x: 1 if x == "NON" else 0).sum()
-        nearby_stations_full = (date_filtered_nearby_stations['numbikesavailable'] == 0).sum()
-        nearby_stations_empty = (date_filtered_nearby_stations['numdocksavailable'] == 0).sum()
-
-        current_bike_data.loc[index, 'nearby_stations_closed'] = nearby_stations_closed
-        current_bike_data.loc[index, 'nearby_stations_full'] = nearby_stations_full
-        current_bike_data.loc[index, 'nearby_stations_empty'] = nearby_stations_empty
-
-        total_nearby_stations = len(date_filtered_nearby_stations)
-        if total_nearby_stations > 0:
-            likelihood_fill = nearby_stations_full / total_nearby_stations
-            likelihood_empty = nearby_stations_empty / total_nearby_stations
+        if nearby_stations is None or len(nearby_stations) < 5:
+            selected_features = base_features
         else:
-            likelihood_fill = 0.0
-            likelihood_empty = 0.0
+            station_data = station_data.copy()
+            station_data['nearby_stations_closed'] = nearby_stations['is_renting'].apply(lambda x: 1 if x == 'NON' else 0).sum()
+            station_data['nearby_stations_full'] = (nearby_stations['numbikesavailable'] == 0).sum()
+            station_data['nearby_stations_empty'] = (nearby_stations['numdocksavailable'] == 0).sum()
+            station_data['likelihood_fill'] = station_data['nearby_stations_full'] / (len(nearby_stations) + 1e-5)
+            station_data['likelihood_empty'] = station_data['nearby_stations_empty'] / (len(nearby_stations) + 1e-5)
+            selected_features = base_features + additional_features
 
-        current_bike_data.loc[index, 'likelihood_fill'] = likelihood_fill
-        current_bike_data.loc[index, 'likelihood_empty'] = likelihood_empty
+        # Debug: Check if selected features exist in the data
+        missing_features = [feat for feat in selected_features if feat not in station_data.columns]
+        if missing_features:
+            print(f"Missing features for station {station}: {missing_features}")
+            continue
 
-        if nearby_stations_full > total_nearby_stations / 2:
-            current_bike_data.loc[index, 'likelihood_fill'] *= 1.5
-        if nearby_stations_empty > total_nearby_stations / 2:
-            current_bike_data.loc[index, 'likelihood_empty'] *= 1.5
+        # Handle infinite and large values
+        # station_data = handle_infinite_and_large_values(station_data)
 
-print("Loading scaler and models...")
-scaler_path = get_absolute_path('../data/scaler_final_new.pkl')
-model_path = get_absolute_path('../data/model_final_new.pkl')
+        station_data[selected_features] = scaler.fit_transform(station_data[selected_features])
 
-with open(scaler_path, 'rb') as f:
-    scalers = pickle.load(f)
+        X = station_data[selected_features]
+        y = station_data[target]
 
-with open(model_path, 'rb') as f:
-    models = pickle.load(f)
+        if len(X) < 2:
+            continue
 
-current_bike_data['avg_bikes_hour_day'] = current_bike_data.groupby(['stationcode', 'hour', 'day_of_week'])['numbikesavailable'].transform('mean')
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-feature_names_file_path = get_absolute_path('../data/feature_names.json')
-with open(feature_names_file_path, 'r') as f:
-    feature_names = json.load(f)
+        if len(X_train) == 0 or len(X_test) == 0:
+            continue
 
-current_bike_data[feature_names] = current_bike_data[feature_names].fillna(0)
+        n_splits = min(5, len(X_train))
+        cv = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+        random_search = RandomizedSearchCV(RandomForestRegressor(random_state=42), param_distributions=param_dist, n_iter=20, cv=cv, scoring='neg_mean_squared_error', n_jobs=-1)
+        random_search.fit(X_train, y_train)
+        best_model = random_search.best_estimator_
+        cv_score = cross_val_score(best_model, X_train, y_train, cv=cv, scoring='neg_mean_squared_error')
+        print(f"Cross-Validation Score for station {station}: {cv_score.mean()}")
+        best_model.fit(X_train, y_train)
+        y_pred = best_model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"Mean Squared Error for station {station}: {mse}")
 
-current_bike_data.replace([np.inf, -np.inf], np.nan, inplace=True)
-current_bike_data.fillna(0, inplace=True)
+        models[station] = best_model
 
-print("Making predictions and comparing with actual values...")
-results = []
+    return models, scaler
 
-for station in tqdm(models.keys(), desc="Predicting for each station"):
-    station_data = current_bike_data[current_bike_data['stationcode'] == station].copy()
-    if station_data.empty:
-        continue
+def save_intermediate_results(models, scalers, batch_number):
+    model_file_path = get_absolute_path(f'../data/model_batch_{batch_number}.pkl')
+    scaler_file_path = get_absolute_path(f'../data/scaler_batch_{batch_number}.pkl')
 
-    scaler = scalers.get(station)
-    if not scaler:
-        print(f"No scaler found for station {station}")
-        continue
+    with open(model_file_path, 'wb') as f:
+        pickle.dump(models, f)
+    with open(scaler_file_path, 'wb') as f:
+        pickle.dump(scalers, f)
+    print(f"Intermediate results saved for batch {batch_number}")
 
-    station_data_features = station_data[feature_names]
+def load_intermediate_results(batch_number):
+    model_file_path = get_absolute_path(f'../data/model_batch_{batch_number}.pkl')
+    scaler_file_path = get_absolute_path(f'../data/scaler_batch_{batch_number}.pkl')
 
-    scaled_features = scaler.transform(station_data_features)
-    station_data[feature_names] = scaled_features
+    if os.path.exists(model_file_path) and os.path.exists(scaler_file_path):
+        with open(model_file_path, 'rb') as f:
+            models = pickle.load(f)
+        with open(scaler_file_path, 'rb') as f:
+            scalers = pickle.load(f)
+        return models, scalers
+    return {}, {}
 
-    X = station_data[feature_names]
-    y_true = station_data['numbikesavailable']
-    model = models[station]
-    y_pred = model.predict(X)
-    station_data['predicted_bikesavailable'] = y_pred
-    station_data['actual_bikesavailable'] = y_true
-    results.append(station_data)
+def main():
+    processed_data_file_path = get_absolute_path('../data/processed_bike_data.parquet')
+    bike_data = pd.read_parquet(processed_data_file_path)
+    
+    if bike_data.empty:
+        raise ValueError("The processed bike data is empty. Please check the Parquet file and its loading process.")
+    
+    # Batch process stations in chunks of 100
+    stations = bike_data['stationcode'].unique()
+    batch_size = 100
 
-results_df = pd.concat(results)
+    all_models = {}
+    all_scalers = {}
 
-results_df = results_df.drop(columns=['date', 'lat', 'lon'])
+    # Load intermediate results if they exist
+    current_batch = 0
+    while os.path.exists(get_absolute_path(f'../data/model_batch_{current_batch}.pkl')):
+        models, scalers = load_intermediate_results(current_batch)
+        all_models.update(models)
+        all_scalers.update(scalers)
+        current_batch += 1
 
-results_df['hour_unscaled'] = current_bike_data['hour_unscaled']
-results_df['day_of_week_unscaled'] = current_bike_data['day_of_week_unscaled']
+    for i in range(current_batch * batch_size, len(stations), batch_size):
+        batch_stations = stations[i:i + batch_size]
+        models, scaler = train_model(bike_data, batch_stations)
+        all_models.update(models)
+        all_scalers.update({station: scaler for station in batch_stations})
+        
+        # Save intermediate results
+        save_intermediate_results(models, {station: scaler for station in batch_stations}, current_batch)
+        current_batch += 1
 
-results_df = results_df[['stationcode', 'name', 'is_installed', 'capacity', 'numdocksavailable', 'numbikesavailable', 'mechanical', 'ebike', 'is_renting', 'is_returning', 'coordonnees_geo','predicted_bikesavailable', 'actual_bikesavailable', 'hour_unscaled', 'day_of_week_unscaled']]
+    # Save the final combined results
+    scaler_file_path = get_absolute_path('../data/scaler_final.pkl')
+    model_file_path = get_absolute_path('../data/model_final.pkl')
 
-results_file_path = get_absolute_path('../data/predictions_vs_actuals_final.pkl')
-with open(results_file_path, 'wb') as f:
-    pickle.dump(results_df, f)
+    with open(scaler_file_path, 'wb') as f:
+        pickle.dump(all_scalers, f)
+    print(f"Scaler saved to {scaler_file_path}")
 
-print("Predictions and actual values saved to", results_file_path)
+    with open(model_file_path, 'wb') as f:
+        pickle.dump(all_models, f)
+    print(f"Models saved to {model_file_path}")
+
+    print("Model training complete.")
+
+if __name__ == "__main__":
+    main()
